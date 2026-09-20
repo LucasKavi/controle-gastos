6b80c0073b84b97f5f0a8b42a6a9729172d63e6e
Lucas <l.aalbuquerque@hotmail.com>
2026-09-20 14:56:28 -0300

    Corrigindo versao 20setembro_2tentativa

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby-VhAJvAVKBdIWrdM4T39CEU1RdnksKzI_ujd_jXPcN8Yt8t_3nWqZh8VbMTxMyqoi5w/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz1ByHSj8YUKCTDLloEdLHNGyv2IOlME0ZSyjMr1HS0aV_kwOH73ZizkvPwgzPzojCppg/exec";
 let usuarioAtual = null;
 let senhaAtual = null;
 
